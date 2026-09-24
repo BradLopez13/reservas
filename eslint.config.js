455bfe500ade9c79f10637cc9defcc9b18adcddf
Brad Alejandro López Basantes <bradlopezbasantes@gmail.com>
2026-09-24 18:49:52 +0200

    Scaffold pnpm monorepo

---
+++ eslint.config.js
@@ -0,0 +1,7 @@
+import tseslint from 'typescript-eslint';
+
+export default tseslint.config(
+  { ignores: ['**/dist/**', '**/node_modules/**', '**/drizzle/**', 'apps/web/src/components/react-bits/**'] },
+  ...tseslint.configs.recommended,
+  { rules: { '@typescript-eslint/consistent-type-imports': 'error' } },
+);
